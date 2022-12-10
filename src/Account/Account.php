e69f0721ebfb623a959c6e0ce9f7da2395483ed3
Maximilian Osinski <maximilianosinski@maxeman.com>
2022-12-10 13:25:03 +0100

    Improved Account.php

--- src/Account/Account.php
+++ src/Account/Account.php
@@ -11,11 +11,13 @@ use Plate\PlateFramework\Exceptions\NotFoundException;
 
 class Account {
 
+    private Database $database;
     public Details $details;
     public object $data;
 
-    private function __construct(Details $details, object $data)
+    private function __construct(Database $database, Details $details, object $data)
     {
+        $this->database = $database;
         $this->details = $details;
         $this->data = $data;
     }
@@ -36,6 +38,7 @@ class Account {
     public static function create(Database $database, ?string $first_name, ?string $last_name, string $email, string $password): self
     {
         if(Email::exists($database, $email)) throw new ConflictException("E-Mail already exists.");
+        if(!strlen($password) >= 8) throw new BadRequestException("Password is too short.");
         $uuid = uniqid();
         $password = password_hash($password, PASSWORD_DEFAULT);
         $query = "INSERT INTO ".$database->databaseTableConfig["ACCOUNTS"]."(uuid, first_name, last_name, email, confirmed, password) VALUES(:uuid, :first_name, :last_name, :email, false, :password)";
@@ -88,7 +91,86 @@ class Account {
             unset($result->confirmed);
             unset($result->password);
 
-            return new self($details, $result);
+            return new self($database, $details, $result);
         } throw new NotFoundException("Account doesn't exist.");
+    }
+
+    /**
+     * Sets a property for the current account.
+     * @param string $property
+     * @param mixed $value
+     * @return bool
+     * @throws BadRequestException
+     * @throws InternalServerException
+     */
+    public function setProperty(string $property, mixed $value): bool
+    {
+        if(!property_exists(Details::class, $property)) {
+            if(!empty($value)) {
+                $query = "UPDATE ".$this->database->databaseTableConfig["ACCOUNTS"]." SET $property = :value WHERE uuid = :uuid";
+                $result = $this->database->execute($query, ["value" => $value, "uuid" => $this->details->uuid]);
+                if($result) {
+                    $this->data[$property] = $value;
+                    return true;
+                } throw new InternalServerException("Couldn't set property.");
+            } throw new BadRequestException("Can't set property with empty value.");
+        } throw new BadRequestException("Can't access detail properties.");
+    }
+
+    /**
+     * Unsets a property for the current account.
+     * @param string $property
+     * @return bool
+     * @throws BadRequestException
+     * @throws InternalServerException
+     */
+    public function unsetProperty(string $property): bool
+    {
+        if(!property_exists(Details::class, $property)) {
+            $query = "UPDATE ".$this->database->databaseTableConfig["ACCOUNTS"]." SET $property = null WHERE uuid = :uuid";
+            $result = $this->database->execute($query, ["uuid" => $this->details->uuid]);
+            if($result) {
+                unset($this->data[$property]);
+                return true;
+            } throw new InternalServerException("Couldn't unset property.");
+        } throw new BadRequestException("Can't access detail properties.");
+    }
+
+    /**
+     * Sets the email for the current account.
+     * @param string $email
+     * @return bool
+     * @throws BadRequestException
+     * @throws ConflictException
+     * @throws InternalServerException
+     */
+    public function setEmail(string $email): bool
+    {
+        if(!Email::exists($this->database, $email)) throw new ConflictException("E-Mail already exists.");
+        $query = "UPDATE ".$this->database->databaseTableConfig["ACCOUNTS"]." SET email = :email WHERE uuid = :uuid";
+        $result = $this->database->execute($query, ["email" => $email, "uuid" => $this->details->uuid]);
+        if($result) {
+            $this->details->email = $email;
+            return true;
+        } throw new InternalServerException("Couldn't set email.");
+    }
+
+    /**
+     * Sets the password for the current account.
+     * @param string $password
+     * @return bool
+     * @throws BadRequestException
+     * @throws InternalServerException
+     */
+    public function setPassword(string $password): bool
+    {
+        if(!strlen($password) >= 8) throw new BadRequestException("Password is too short.");
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $query = "UPDATE ".$this->database->databaseTableConfig["ACCOUNTS"]." SET password = :email WHERE uuid = :uuid";
+        $result = $this->database->execute($query, ["password" => $password, "uuid" => $this->details->uuid]);
+        if($result) {
+            $this->details->password = $password;
+            return true;
+        } throw new InternalServerException("Couldn't set password.");
     }
 }
